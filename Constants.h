728219d2cad27ff60873307549430ab17fe8e884
luki120 <luki120@exerra.xyz>
2021-12-28 02:10:44 -0300

    constants file

---
+++ Constants.h
@@ -0,0 +1,6 @@
+@import UIKit;
+
+
+static NSString *const kPath = @"/var/mobile/Library/Preferences/me.luki.arizonaprefs.plist";
+
+#define AriTintColor [UIColor colorWithRed: 0.02 green: 0.79 blue: 0.95 alpha: 1.0]
